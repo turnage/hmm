02ee5c29f0fdb0b555f1f5350b4af8834d719477
Payton Turnage <paytonturnage@gmail.com>
2017-04-01 17:38:20 -0400

    rustdoc base constructor.

--- src/base/mod.rs
+++ src/base/mod.rs
@@ -1,3 +1,5 @@
+//! Base implements algorithms shared for all or many hmm architectures.
+
 mod matrix;
 
 use self::matrix::Matrix;
@@ -9,6 +11,17 @@ pub struct Base {
 }
 
 impl Base {
+    /// from returns a Base hmm from the initial distribution of hidden states, probability matrix
+    /// of hidden state transitions, and probability distributions of emissions from each hidden
+    /// state (traditionally denoted pi, a, and b respectively).
+    ///
+    /// The provided matrices must be row-stochastic. Example format:
+    ///
+    ///     init: [0.6 0.4]
+    ///     trans: [0.7 0.3 // trans[i][j] is the probability of transitioning from state i to j.
+    ///             0.4 0.6]
+    ///     emit: [0.1 0.4 0.5  // each column is an emission; each row is a state's emission dist.
+    ///            0.7 0.2 0.1]
     pub fn from(init: Matrix, trans: Matrix, emit: Matrix) -> Result<Self, String> {
         let check = &|valid, error| if valid { Ok(()) } else { Err(error) };
         let ((ir, ic), (tr, tc), (er, ec)) = (init.dims(), trans.dims(), emit.dims());
