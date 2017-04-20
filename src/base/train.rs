930635393cef1d4dda9081186759488ae8b94937
Payton Turnage <paytonturnage@gmail.com>
2017-04-20 17:14:24 -0400

    Implement discrete hmm training.

--- src/base/train.rs
+++ src/base/train.rs
@@ -1,50 +1,117 @@
-use base::Model;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+use std::hash::Hash;
 
-pub struct Multinomial {
-    n: usize,
-    m: usize,
-    model: Model<usize, usize, Vec<f64>, Vec<Vec<f64>>, Vec<Vec<f64>>>,
+use base::{Model, Starter, Emitter, Transor};
+
+struct Discrete<S: Copy, O: Copy> {
+    start: HashMap<S, f64>,
+    emit: HashMap<S, HashMap<O, f64>>,
+    trans: HashMap<S, HashMap<S, f64>>,
 }
 
-impl Multinomial {
-    pub fn new(n: usize, m: usize) -> Self {
-        Multinomial {
-            n: n,
-            m: m,
-            model: Model::from(Vec::new(), Vec::new(), Vec::new()),
+impl<S: Copy + Eq + Hash, O: Copy + Eq + Hash> Discrete<S, O> {
+    fn new() -> Self {
+        Discrete {
+            start: HashMap::new(),
+            emit: HashMap::new(),
+            trans: HashMap::new(),
         }
     }
 
-    /// learn trains the model on sequences, which are pairs of (hidden state, emission)
-    /// so that it can predict similar sequences.
-    pub fn learn(n: usize,
-                 m: usize,
-                 seqs: Vec<Vec<(usize, usize)>>)
-                 -> Model<usize, usize, Vec<f64>, Vec<Vec<f64>>, Vec<Vec<f64>>> {
-        let mut starts = vec![0f64; n];
-        let mut emit = vec![vec![0f64; m]; n];
-        let mut trans = vec![vec![0f64; n]; n];
-        let mut prev: Option<usize> = None;
-        for seq in seqs.iter() {
-            let (state, _) = seq[0];
-            starts[state] += 1f64;
-            for (i, &(state, emission)) in seq.iter().enumerate() {
-                if let Some(prev) = prev {
-                    trans[prev][state] += 1f64;
-                }
-                emit[state][emission] += 1f64;
-                prev = Some(state);
+    fn model
+        (self)
+         -> Model<S, O, HashMap<S, f64>, HashMap<S, HashMap<O, f64>>, HashMap<S, HashMap<S, f64>>> {
+        Model::from(self.start, self.emit, self.trans)
+    }
+
+    fn learn_state_path(&mut self, points: &Vec<(S, O)>) {
+        self.observe_state_path(points);
+        self.stochast();
+    }
+
+    fn observe_state_path(&mut self, points: &Vec<(S, O)>) {
+        let &(start_state, _) = points.first().unwrap();
+        *self.start.entry(start_state).or_insert(0f64) += 1f64;
+
+        let mut prev = None;
+        for &(state, obs) in points.iter() {
+            *self.emit
+                .entry(state)
+                .or_insert(HashMap::new())
+                .entry(obs)
+                .or_insert(0f64) += 1f64;
+            if let Some(prev) = prev {
+                *self.trans
+                    .entry(prev)
+                    .or_insert(HashMap::new())
+                    .entry(state)
+                    .or_insert(0f64) += 1f64;
+            }
+            prev = Some(state);
+        }
+    }
+
+    fn stochast(&mut self) {
+        let start_denom = self.start.values().sum::<f64>();
+        for (_, pi) in self.start.iter_mut() {
+            *pi /= start_denom;
+        }
+
+        for (_, dist) in self.emit.iter_mut() {
+            let dist_denom = dist.values().sum::<f64>();
+            for (_, val) in dist.iter_mut() {
+                *val /= dist_denom;
             }
         }
 
-        Model::from(Multinomial::stochast(&starts),
-                    emit.iter().map(|dist| Multinomial::stochast(dist)).collect(),
-                    trans.iter().map(|dist| Multinomial::stochast(dist)).collect())
+        for (_, dist) in self.trans.iter_mut() {
+            let dist_denom = dist.values().sum::<f64>();
+            for (_, val) in dist.iter_mut() {
+                *val /= dist_denom;
+            }
+        }
+    }
+}
+
+impl<S: Copy + Eq + Hash> Starter<S> for HashMap<S, f64> {
+    fn startp(&self, s: S) -> Result<f64, String> {
+        if let Some(&p) = self.get(&s) {
+            Ok(p)
+        } else {
+            Err(format!("state not found"))
+        }
+    }
+}
+
+impl<S: Copy + Eq + Hash, O: Copy + Eq + Hash> Emitter<S, O> for HashMap<S, HashMap<O, f64>> {
+    fn emitp(&self, s: S, o: O) -> Result<f64, String> {
+        if let Some(dist) = self.get(&s) {
+            if let Some(&p) = dist.get(&o) {
+                Ok(p)
+            } else {
+                Ok(0f64)
+            }
+        } else {
+            Err(format!("state not found"))
+        }
+    }
+}
+
+impl<S: Copy + Eq + Hash> Transor<S> for HashMap<S, HashMap<S, f64>> {
+    fn transp(&self, a: S, b: S) -> Result<f64, String> {
+        if let Some(dist) = self.get(&a) {
+            if let Some(&p) = dist.get(&b) {
+                Ok(p)
+            } else {
+                Ok(0f64)
+            }
+        } else {
+            Err(format!("state not found"))
+        }
     }
 
-    /// stochast makes a vector stochastic by dividing all elements by the vector sum.
-    fn stochast(v: &Vec<f64>) -> Vec<f64> {
-        let sum = v.iter().sum::<f64>();
-        v.iter().map(|v| v / sum).collect()
+    fn states(&self) -> Vec<S> {
+        self.keys().cloned().collect()
     }
 }
