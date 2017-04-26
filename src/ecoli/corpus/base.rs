7b658494e7a791cf01d66ba3d59680697b3d3271
Payton Turnage <paytonturnage@gmail.com>
2017-04-26 08:44:30 -0400

    Implement dna string -> base vec.

--- src/ecoli/corpus/base.rs
+++ src/ecoli/corpus/base.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 pub enum Base {
     A,
@@ -15,5 +17,22 @@ impl Base {
             'g' => Ok(Base::G),
             x => Err(format!("{} is not a valid dna base", x)),
         }
+    }
+}
+
+pub fn parse(dna: String) -> Result<Vec<Base>, String> {
+    dna.chars().filter(|c| c.is_alphabetic()).map(|c| Base::from(c)).collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        assert_eq!(parse(String::from("ac\nt\ng")),
+                   Ok(vec![Base::A, Base::C, Base::T, Base::G]));
+        assert_eq!(parse(String::from("ac\nt\nyg")),
+                   Err(String::from("y is not a valid dna base")));
     }
 }
